5e9f13584af1fb38cb21a2edbee3b37a5f96db6d
彭斌斌 <binbin@mgtv.com>
2022-02-13 15:32:17 +0800

    小程序 secret

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   default_module: 'api',
   weixin: {
     appid: 'wxc9f99f65f4c2b030', // 小程序 appid
-    secret: '', // 小程序密钥
+    secret: '667b7ea39a50d537f00f9d9374760650', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
